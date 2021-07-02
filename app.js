31f206cb92594fe611880ae3216c2bb74a0a6075
angelicaf13 <figueroa.angelica@uabc.edu.mx>
2021-07-02 00:01:19 -0700

    add procfile and modify listen port in app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log("Servidor corriendo en el puerto: " + port);
 });
 
