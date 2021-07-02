6a07eef4ac0a93eeb7add42b76a38d2bbe52c63c
angelicaf13 <figueroa.angelica@uabc.edu.mx>
2021-07-01 23:41:59 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const port = 3000;
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log("Servidor corriendo en el puerto: " + port);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
